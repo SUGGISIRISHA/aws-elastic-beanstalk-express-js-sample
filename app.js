211ce565cd4b491aa80b5556b0c08a4622a245e7
S.Sirisha <138255075+SUGGISIRISHA@users.noreply.github.com>
2023-09-10 23:40:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Successfully changed from Hii Sri ti Hello Sri'));
+app.get('/', (req, res) => res.send('hellooo'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
